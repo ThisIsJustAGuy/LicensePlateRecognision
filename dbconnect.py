aaab1794742b4ab9eb91c3333b07efa3da5115e8
unknown <bbalint0404@gmail.com>
2022-11-19 10:55:09 +0100

    added check in auth :passport_control:

--- dbconnect.py
+++ dbconnect.py
@@ -2,12 +2,7 @@
 import mysql.connector
 
 
-def connect(hostname, user, password, database):
+def connecttodb(hostname, username, pwd, dbname):
     db = mysql.connector.connect(
-        host=hostname, user="root", password="", database="rendszamok")
-    cursor = db.cursor()
-# query = "SELECT * FROM adatok"
-# cursor.execute(query)
-# for (rendszam, ervenyesberlet, szektor) in cursor:
-#     neme = "" if ervenyesberlet else " nem"
-#     print(f"{rendszam} bérlete{neme} érvényes a '{szektor}' szektorra.")
+        host=hostname, user=username, password=pwd, database=dbname)
+    return db
