ec7f5700e3173d1111e228d0e21b4540091503e9
unknown <bbalint0404@gmail.com>
2022-11-19 13:25:29 +0100

    adds plate comparison modifications for email to work :bug:

--- plate_compare.py
+++ plate_compare.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 from dbconnect import connecttodb
+from send_email import start_email_process
 
 
 def can_car_come_in(plate, sector):
     plate = plate.upper()
     sector = sector.upper()
     cursor.execute(
-        "SELECT * FROM adatok WHERE rendszam = %(plate)s AND szektor = %(sector)s", {"plate": plate, "sector": sector})
+        "SELECT * FROM adatok WHERE rendszam = %(plate)s AND szektor = %(sector)s AND ervenyesberlet = 1", {"plate": plate, "sector": sector})
 
 
 def get_email_by_plate(plate):
@@ -16,11 +17,17 @@ def get_email_by_plate(plate):
 
 
 SECTOR = 'A'
-PLATE = "AAA111"
+PLATE = "AAAA123"
 db = connecttodb("localhost", "root", "", "rendszamok")
 cursor = db.cursor()
 can_car_come_in(PLATE, SECTOR)
 if (cursor.fetchone()):
     print("Itt világít a LED, bejöhet a kocsi")
 else:
+    # itt majd külön kell venni, hogy rossz a szektor, vagy a bérlet járt le, illetve benne van-e egyáltalán a rendszám a db-ben
+    get_email_by_plate(PLATE)
+    dest_email = cursor.fetchone()[0]
+    print(dest_email)
     print("Itt villog a LED, email-t kap az illető")
+    # emailnél a case-el lehet szórakozni
+    start_email_process(dest_email, PLATE)
