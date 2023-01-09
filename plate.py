38451a8ee49848188e1ffdc2b92316a86d3478d2
Fábry László <70687838+fabrylaszlo@users.noreply.github.com>
2023-01-09 19:46:55 +0100

    Add files via upload
    Mátyás felismerés

---
+++ plate.py
@@ -0,0 +1,16 @@
+import requests
+import os
+from pprint import pprint
+
+
+os.system('fswebcam -q  capture.jpg')
+regions = ['hu', 'us-ca'] 
+with open('capture.jpg', 'rb') as fp:
+    response = requests.post(
+        'https://api.platerecognizer.com/v1/plate-reader/',
+        data=dict(regions=regions),
+        files=dict(upload=fp),
+        headers={'Authorization': 'Token 830777e03179f94991f4f24a20351e66ddaf0e5e'})
+
+data=response.json()
+print(data['results'][0]['plate'])
