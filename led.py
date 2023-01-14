66c66c397e04e209f5bdc11b5ef04d473ad67e5d
fabrylaszlo <babo200113@gmail.com>
2023-01-14 13:22:34 +0100

    osszerakott

---
+++ led.py
@@ -0,0 +1,36 @@
+# import time
+# import RPi.GPIO as GPIO
+
+# def green_light():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(23, GPIO.OUT)
+#     GPIO.output(23,True)
+#     time.sleep(5)
+#     GPIO.output(23,False)
+
+# def red_light():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(24, GPIO.OUT)
+#     GPIO.output(24,True)
+#     time.sleep(5)
+#     GPIO.output(24,False)
+    
+# def red_fast():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(24, GPIO.OUT)
+#     for x in range(20):
+#         GPIO.output(24,True)
+#         time.sleep(0.2)
+#         GPIO.output(24,False)
+
+# def red_slow():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     GPIO.setup(24, GPIO.OUT)
+#     for x in range(20):
+#         GPIO.output(24,True)
+#         time.sleep(1)
+#         GPIO.output(24,False)
